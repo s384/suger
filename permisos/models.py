14344ef28f2fd9c5a075fb54137e7e8b0f2e7c58
edgardo pino <camilopt.13.93@gmail.com>
2019-12-23 00:38:17 -0300

    Permisos almost ready validación de botones pendiente

--- permisos/models.py
+++ permisos/models.py
@@ -4,9 +4,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class SolicitudPermisos(models.Model):
-	tipos_permisos = [(0, "Ausencia"),(1, "Permiso Administrativo"),(2,"Vacaciones"),(3,"Licencia Médica"),(4,"Atraso"),(5,"Salida Anticipada")]
-	tipos_jornadas = [(0, "Completa"),(1, "Parcial Mañana"),(2, "Parcial Tarde")]
-	opciones_estado = [(1, "No Revisado"), (2, "En revisión"), (3, "Rechazada"), (4, "Aprovada")]
+	tipos_permisos = [
+		(0,"Ausencia"),
+		(1,"Permiso Administrativo"),
+		(2,"Vacaciones"),
+		(3,"Licencia Médica"),
+		(4,"Atraso"),
+		(5,"Salida Anticipada")
+	]	
+	opciones_estado = [
+		(1, "No Revisado"),
+		(2, "En revisión"),
+		(3, "Rechazada"),
+		(4, "Aprovada")
+	]
 
 	titulo = models.CharField(max_length = 100,verbose_name = "título de la solicitud de permiso")
 	usuario = models.ForeignKey(User, on_delete=models.CASCADE,related_name="usuarios_solicitudes_permisos")
@@ -16,7 +27,7 @@ class SolicitudPermisos(models.Model):
 	dias_permiso = models.PositiveSmallIntegerField(default = 1)
 	horas_permiso = models.PositiveSmallIntegerField(null = True ,blank = True)
 	img_referencia = models.ImageField(upload_to = "Permisos", null = True ,blank = True)
-	estado_solicitud = models.PositiveSmallIntegerField(choices=opciones_estado, default = 2)
+	estado_solicitud = models.PositiveSmallIntegerField(choices=opciones_estado, default = 1)
 
 	created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
 	updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualizacion")
