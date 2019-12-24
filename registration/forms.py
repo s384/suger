cb9f2a6be49b0ccc75c1ca14e09246350e21d42e
MarceloMoralesP <marcelomoralespizarro@gmail.com>
2019-12-23 23:58:22 -0300

    validacion area

--- registration/forms.py
+++ registration/forms.py
@@ -71,6 +71,21 @@ class AreaForm(forms.ModelForm):
 			'boss_user' : forms.Select(attrs={'class':'form-control'}),
 		}
 
+	def clean_nombre(self):
+		nombre = self.cleaned_data.get("nombre")
+		if Area.objects.filter(nombre=nombre).exists():
+			raise forms.ValidationError("Está área ya ha sido creada")
+		return nombre
+
+
+	def clean_boss_user(self):
+		boss_user = self.cleaned_data.get("boss_user")
+		if Area.objects.filter(boss_user=boss_user).exists():
+			raise forms.ValidationError("El usuario seleccionado ya cuenta con jefatura")
+		return boss_user
+
+
+
 class UserActive(forms.ModelForm):
 	class Meta:
 		model = User
