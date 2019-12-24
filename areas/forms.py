8df56b3a631b4e63459ea7d81d760f973c0f771b
s384-asus <seba.alexis.trujillo@gmail.com>
2019-12-24 17:22:30 -0300

    def clean nombre in area update

--- areas/forms.py
+++ areas/forms.py
@@ -32,4 +32,13 @@ class AreaUpdateForm(forms.ModelForm):
             'boss_user' : forms.Select(attrs={'class':'form-control'}),
         }
 
-#Condicionar la modificacion del nombre, para que pueda guardar el mismo nombre si no es cambiado.
+    def clean_boss_user(self):
+        boss_user = self.cleaned_data.get("boss_user")
+        nombre = self.cleaned_data.get("nombre")
+        if Area.objects.filter(nombre=nombre).exists():
+            if Area.objects.filter(boss_user=boss_user).exists():
+                raise forms.ValidationError("El usuario seleccionado ya cuenta con jefatura")
+
+            return boss_user  
+        else:
+            return boss_user     
